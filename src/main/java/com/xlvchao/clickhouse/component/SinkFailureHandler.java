74e9d706a8190d5be1ff4e0e0ef935b89a258eef
xlvchao <33950671+xlvchao@users.noreply.github.com>
2022-09-05 19:00:56 +0800

    Create SinkFailureHandler.java

---
+++ src/main/java/com/xlvchao/clickhouse/component/SinkFailureHandler.java
@@ -0,0 +1,10 @@
+package com.hihonor.aiops.clickhouse.component;
+
+import java.util.List;
+
+/**
+ * Created by lvchao on 2022/9/5 18:13
+ */
+public interface SinkFailureHandler {
+    void handle(List data, Class clazz);
+}
